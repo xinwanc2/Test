99172a2f0b1b9942c52def93c1017f7931b4cb74
xinwanc <38121289+xinwanc@users.noreply.github.com>
2022-04-19 01:25:20 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        Console.WriteLine("Roman Numeral Converter\r");
+        Console.WriteLine("------------------------\n");
+
+        while (endApp is false)
+        {
+
+            Console.Write("Type a number or roman numeral, and then press Enter: ");
+            string inputValue = Console.ReadLine() ?? "";
+
+            if (string.IsNullOrEmpty(inputValue))
+            {
+                Console.WriteLine("Please key in valid input");
+                endApp = true;
+            }
+
+            try
+            {
+                if (Int32.TryParse(inputValue, out int number1))
+                {
+                    string result = RomanIntervalConverter.NumToRoman(number1);
+                    Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                else
+                {
+                    int result = RomanIntervalConverter.RomanToNum(inputValue);
+                    Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+        return;
+    }
+}
---
+++ RomanIntervalConverter.cs
@@ -0,0 +1,101 @@
+﻿namespace Test
+{
+    public class RomanIntervalConverter
+    {
+        private static Dictionary<string, int> RomanNumerals = new()
+        {
+            {
+                "I",
+                1
+            },
+            {
+                "IV",
+                4
+            },
+            {
+                "V",
+                5
+            },
+            {
+                "IX",
+                9
+            },
+            {
+                "X",
+                10
+            },
+            {
+                "XL",
+                40
+            },
+            {
+                "L",
+                50
+            },
+            {
+                "XC",
+                90
+            },
+            {
+                "C",
+                100
+            },
+            {
+                "CD",
+                400
+            },
+            {
+                "D",
+                500
+            },
+            {
+                "CM",
+                900
+            },
+            {
+                "M",
+                1000
+            }
+        };
+
+        public static int RomanToNum(string value)
+        {
+            int total = 0;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                string currentRomanChar = value[i] + "";
+                RomanNumerals.TryGetValue(currentRomanChar, out int num);
+                if (i + 1 < value.Length && RomanNumerals[value[i + 1] + ""] > RomanNumerals[currentRomanChar])
+                {
+                    total -= num;
+                }
+                else
+                {
+                    total += num;
+                }
+            }
+            return total;
+        }
+
+        public static string NumToRoman(int value)
+        {
+            string result = "";
+
+            foreach (var item in RomanNumerals.Reverse())
+            {
+                if (value <= 0)
+                {
+                    break;
+                };
+
+                while (value >= item.Value)
+                {
+                    result += item.Key;
+                    value -= item.Value;
+                }
+            }
+            return result;
+        }
+    }
+}
